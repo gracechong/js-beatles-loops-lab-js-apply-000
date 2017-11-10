35c6d1796bf22db4f557b2ff37bc51317d9cb8d0
Grace Chong <gracechong423@gmail.com>
2017-11-10 02:55:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musicians, instruments) {
+  var result = [];
+
+  for (var i = 0; i < musicians.length; i++) {
+    result.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return result;
+}
